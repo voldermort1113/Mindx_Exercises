5f000892429654a539b1cda0ed6f347e8216ba70
linh37 <voldermort1111@gmail.com>
2019-07-21 21:43:37 +0700

    .

--- Session 5/seriousExercises/is_inside/index.js
+++ Session 5/seriousExercises/is_inside/index.js
@@ -1,11 +1,10 @@
 const is_inside = ([x,y],[recX,recY,recWidth,recHeight])=>{    
-    let point = [x,y]; //điểm có tọa độ x,y
-    let rectangle = [recX,recY,recWidth,recHeight];
+
     if ((x>=recX && x<=(recX+recWidth))&&(y>=recY && y<=(recY+recHeight))){
         return true;
     }else{
         return false;
     }
 }
-console.log( is_inside([100, 120], [140, 60, 	]) );
-console.log( is_inside([200, 120], [140,60,100,200]) );
+console.log( is_inside([100, 120], [140, 60, 70, 50]) );
+console.log( is_inside([200, 120], [140, 60, 100, 200]) );
